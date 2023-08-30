0ddc37333406a2568d82fabbba7fd88603930a2a
Ginigal Godage Vimukthi Pahasara <vimukthipahasara.gg@gmail.com>
2023-08-31 02:17:20 +0530

    Added some features to see the working of code earlier

--- Experiments/accuracy_functions.py
+++ Experiments/accuracy_functions.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 def average_precision(gtp_positions):
+    if gtp_positions.shape[0] == 0:
+        return 0
     num_images = torch.tensor(np.array([i for i in range(1, gtp_positions.shape[0] + 1)]))
     fractions = torch.div(num_images, gtp_positions)
     sum_of_fractions = torch.sum(fractions)
