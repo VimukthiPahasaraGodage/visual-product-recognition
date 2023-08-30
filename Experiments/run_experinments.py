257386c96812d6c4f88e663ea2827104db1b659f
Ginigal Godage Vimukthi Pahasara <vimukthipahasara.gg@gmail.com>
2023-08-30 12:22:15 +0530

    Downgraded the model to eliminate the OOM in GPU

--- Experiments/run_experinments.py
+++ Experiments/run_experinments.py
@@ -1,4 +1,5 @@
 from experiment import Experiment
+from models.model1_v1 import VitModels
 
 training_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/train_dataset.csv'
 validation_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/validation_dataset.csv'
@@ -22,6 +23,8 @@ experiment1 = Experiment(
     train_validation_image_dir,
     query_image_dir,
     gallery_image_dir,
+    vit_model=VitModels.ViT_B_16,
+    linear_layer_output_dim=768,
     num_epochs=1)
 
 experiment1.train()
