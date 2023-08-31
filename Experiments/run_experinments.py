d959425ba2fc884ef99a088ee2705c4d91e9676f
Ginigal Godage Vimukthi Pahasara <vimukthipahasara.gg@gmail.com>
2023-08-31 06:53:42 +0530

    Finalized MVP version 2

--- Experiments/run_experinments.py
+++ Experiments/run_experinments.py
@@ -2,8 +2,8 @@ from experiment import Experiment
 from models.model1_v1 import DistanceMeasures
 from models.model1_v1 import VitModels
 
-training_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/train_dataset_tiniest.csv'
-validation_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/validation_dataset_tiniest.csv'
+training_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/train_dataset_small.csv'
+validation_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/validation_dataset_small.csv'
 query_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/test_queries.csv'
 gallery_dataset_path = '/home/group15/VPR/Project_Code/Data Preprocessing/generated_datasets/test_gallery.csv'
 train_validation_image_dir = '/home/group15/VPR/train_validation_images'
@@ -53,7 +53,7 @@ match experiment_number:
             vit_model=VitModels.ViT_B_16,
             linear_layer_output_dim=768,
             distance_measure=DistanceMeasures.EUCLIDEAN,
-            learning_rate=0.05,
+            learning_rate=0.1,
             batch_size=32,
             num_epochs=3)
 
